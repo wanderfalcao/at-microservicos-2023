d74632325e717193a9faf115cfd1cd3412f42d66
wanderfalcao <33440362+wanderfalcao@users.noreply.github.com>
2023-04-13 12:03:46 -0300

    admin

--- administrator-service/src/test/java/br/com/infnet/administratorservice/AdministratorServiceApplicationTests.java
+++ administrator-service/src/test/java/br/com/infnet/administratorservice/AdministratorServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AdministratorServiceApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws  UnsupportedOperationException {
     }
 
 }
